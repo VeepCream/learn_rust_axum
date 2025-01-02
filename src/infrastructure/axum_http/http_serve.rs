86d192e474856be37093c6898b514feaa7d99e81
veerapat preechapant <veerapat@thelivingos.com>
2025-01-02 12:30:24 +0700

    เริ่มร่ายคาถาเปิดมิติสู่ต่างโลกด้วย HTTP ด้วย Rust Axum

---
+++ src/infrastructure/axum_http/http_serve.rs
@@ -0,0 +1,67 @@
+use anyhow::Result;
+use axum::{http::Method, routing::get, Router};
+use tokio::net::TcpListener;
+use tracing::info;
+use std::{net::SocketAddr, sync::Arc, time::Duration};
+use tower_http::{
+    cors::{Any, CorsLayer},
+    limit::RequestBodyLimitLayer,
+    timeout::TimeoutLayer,
+    trace::TraceLayer,
+};
+
+use crate::{
+    config::config_model::DotEnvyConfig, infrastructure::postgres::postgres_connection::PgPoolSquad,
+};
+
+use super::default_routers;
+
+pub async fn start(config: Arc<DotEnvyConfig>, db_pool: Arc<PgPoolSquad>) -> Result<()> {
+    let app = Router::new()
+        .fallback(default_routers::not_found)
+        .route("/health", get(default_routers::health_check))
+        .layer(TimeoutLayer::new(Duration::from_secs(5)))
+        .layer(TraceLayer::new_for_http())
+        .layer(RequestBodyLimitLayer::new(
+            config.server.body_limit.try_into()?,
+        ))
+        .layer(
+            CorsLayer::new()
+                .allow_methods([
+                    Method::GET,
+                    Method::POST,
+                    Method::DELETE,
+                    Method::PUT,
+                    Method::PATCH,
+                ])
+                .allow_origin(Any),
+        )
+        .layer(TraceLayer::new_for_http());
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], config.server.port));
+
+    let listener = TcpListener::bind(addr).await?;
+
+    info!("Server is running on port: {}", addr);
+
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await?;
+
+    Ok(())
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Failed to install CTRL+C signal handler");
+    };
+     let terminate = std::future::pending::<()>();
+
+     tokio::select! {
+        _ = ctrl_c => info!("Shutting down due to CTRL+C"),
+        _ = terminate => info!("Shutting down due to Termination"),
+     }
+
+}
